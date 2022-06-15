1a9fde3f23fb4f292ff078063884e535484a2048
hungnv <hungnv@hybrid-technologies.vn>
2022-06-15 16:46:19 +0700

    Update code

--- alomf.py
+++ alomf.py
@@ -1,37 +1,47 @@
-def replaceStringReverse(string,num):
-    if num == 0: return string
-    stringInReverse = string[:-num] + "B"*num
+import os
+
+def replace_string_reverse(string, num):
+    if num == 0:
+        return string
+    stringInReverse = string[:-num] + "B" * num
     return stringInReverse
 
-def replaceStringForward(string,num):
+
+def replaceStringForward(string, num):
     stringForward = string
-    stringForward = "B"*num + stringForward[num:]
+    stringForward = "B" * num + stringForward[num:]
     return stringForward
 
-def replaceStringToA(string,num):
+
+def replaceStringToA(string, num):
     replacedString = string
-    replacedString = string[:num] + "A" + string[num+1:]
+    replacedString = string[:num] + "A" + string[num + 1:]
     return replacedString
 
-def stringAB(n,k):
+
+def stringAB(n, k):
     dapAn = []
     dapAnDung = []
     dapAnDung = []
-    fullAString = "A"*n
+    fullAString = "A" * n
     dapAn.append(fullAString)
-    for i in range(n-k+1):
+    for i in range(n - k + 1):
         subjectString = fullAString
-        subjectString = replaceStringReverse(subjectString,(n-k)-i)
-        subjectString = replaceStringForward(subjectString,i)
+        subjectString = replace_string_reverse(subjectString, (n - k) - i)
+        subjectString = replaceStringForward(subjectString, i)
         dapAn.append(subjectString)
 
     for i in dapAn:
         for j in range(len(i)):
-            if i[j] == "B" :
-                dapAn.append(replaceStringToA(i,j))
+            if i[j] == "B":
+                dapAn.append(replaceStringToA(i, j))
     dapAn = list(set(dapAn))
     for i in dapAn:
-        if "A"*(k+1) not in i and i.count("A"*k) == 1:
+        if "A" * (k + 1) not in i and i.count("A" * k) == 1:
             dapAnDung.append(i)
     return sorted(dapAnDung)
-print(stringAB(7,4))
+
+
+print(stringAB(7, 4))
+print(stringAB(7, 4))
+print(stringAB(7, 4))
